e841a01c097795d395833f1e86264daf923b6a61
xBalbinus <xiangan9@gmail.com>
2024-08-01 07:36:27 -0700

    remove entrypoint

--- modules/4337/src/deploy/mock.ts
+++ modules/4337/src/deploy/mock.ts
@@ -1,7 +1,5 @@
 import { DeployFunction } from 'hardhat-deploy/types'
 
-const ENTRY_POINT = process.env.DEPLOYMENT_ENTRY_POINT_ADDRESS
-
 const deploy: DeployFunction = async ({ deployments, getNamedAccounts, network }) => {
   if (!network.tags.test) {
     return
